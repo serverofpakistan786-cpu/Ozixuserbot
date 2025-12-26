ba62e015e482dc8299c6b751a1230bb56a8ba420
serverofpakistan786-cpu <serverofpakistan786@gmail.com>
2025-12-26 17:30:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+
+OWNER_ID = 123456789
+OWNER_NAME = "King Developer"
+
+SUPPORT_CHANNEL = "https://t.me/YourSupportChannel"
+GROUP_LINK = "https://t.me/YourGroupLink"
